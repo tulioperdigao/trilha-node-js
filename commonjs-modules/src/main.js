fb63c13506f07271502bb8ff7d5ec1821596af7d
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-18 12:03:39 -0300

    export consts

--- commonjs-modules/src/main.js
+++ commonjs-modules/src/main.js
@@ -1,10 +1,15 @@
 const p = require("./services/products")
+const config = require("./services/config")
 
 async function main() {
     console.log("Carrinho de compras:");
-    p.getFullName("408", "mousepad");
-    p.getFullName("565", "mouse");
-    p.getProductLabel("Teclado Gamer");
+
+    // p.getFullName("408", "mousepad");
+    // p.getFullName("565", "mouse");
+    // p.getProductLabel("Teclado Gamer");
+
+    console.log(config.devArea);
+    console.log(config.client)
 }
 
 main();
