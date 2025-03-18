6429e705618caa0852e29c52ee18e7e730e3e206
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-18 11:50:13 -0300

    multiple exports

--- commonjs-modules/src/main.js
+++ commonjs-modules/src/main.js
@@ -3,7 +3,8 @@ const p = require("./services/products")
 async function main() {
     console.log("Carrinho de compras:");
     p.getFullName("408", "mousepad");
-    p.getFullName("565", "mouse")
+    p.getFullName("565", "mouse");
+    p.getProductLabel("Teclado Gamer");
 }
 
 main();
--- commonjs-modules/src/services/products.js
+++ commonjs-modules/src/services/products.js
@@ -1,10 +1,15 @@
 // todas as funções que lidam com produto
 
 async function getFullName(codeId, productName) {
-    console.log("\n")
+    console.log("\n");
     console.log("productX " + codeId + "--" + productName);
 }
 
+async function getProductLabel(productName) {
+    console.log("Product " + productName);
+}
+
 module.exports = {
-    getFullName
+    getFullName, 
+    getProductLabel,
 }
