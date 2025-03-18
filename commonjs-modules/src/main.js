0d5f6879abfbc465f1defd5fae630a2f06ccf6cb
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-18 14:28:22 -0300

    export default

--- commonjs-modules/src/main.js
+++ commonjs-modules/src/main.js
@@ -1,5 +1,6 @@
 const product = require("./services/products")
 const config = require("./services/config")
+const dataBase = require("./services/database")
 
 async function main() {
     console.log("Carrinho de compras:");
@@ -8,10 +9,12 @@ async function main() {
     // p.getFullName("565", "mouse");
     // p.getProductLabel("Teclado Gamer");
 
-    product.getFullName("568", "Caixa de madeira");
+    // product.getFullName("568", "Caixa de madeira");
 
-    console.log(config.devArea);
-    console.log(config.client)
+    // console.log(config.devArea);
+    // console.log(config.client);
+
+    dataBase.connectToDatabase("MongoDB");
 }
 
 main();
---
+++ commonjs-modules/src/services/database.js
@@ -0,0 +1,11 @@
+//export default
+
+exports.connectToDatabase = (dataName) => {
+    console.log("Conectando ao banco: " + dataName);
+};
+
+exports.disconnectDataBase = () => {
+    //lógica
+
+    console.log("Desconectando...")
+}
