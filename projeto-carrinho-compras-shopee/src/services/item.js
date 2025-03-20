004d160d02c89c43b33dcaf912093ee55e45ec8d
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-20 15:50:06 -0300

    implementando logica do item.js

--- projeto-carrinho-compras-shopee/src/services/item.js
+++ projeto-carrinho-compras-shopee/src/services/item.js
@@ -2,5 +2,12 @@
 
 // -> Criar item com subtotal calculado
 async function createItem(name, price, quantity) {
-    
+    return {
+        name,
+        price,
+        quantity,
+        subtotal: () => price * quantity,
+    }
 }
+
+export default createItem;
