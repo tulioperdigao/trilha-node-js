c602a94df7486835958c1ba01f57b419a9ffaed4
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-20 16:22:29 -0300

    cartAdd e subtotal

--- projeto-carrinho-compras-shopee/src/services/cart.js
+++ projeto-carrinho-compras-shopee/src/services/cart.js
@@ -2,22 +2,28 @@
 
 // CASOS DE USO
 // -> adicionar Item no carrinho
-async function addItem(userCart) {
-    
-}
+async function addItem(userCart, item) {
+    userCart.push(item);
+};
+
+// -> calcular o total
+async function calculateTotal(userCart) {
+    return userCart.reduce((total, item) => total + item.subtotal(), 0);
+};
 
 // -> remover um Item - diminui um item
 async function removeItem(userCart, index) {
     
-}
+};
 
 // -> deletar Item do carrinho
 async function deleteItem(userCart, nameItem) {
     
-}
+};
 
-// -> calcular o total
-async function calculateTotal(userCart) {
-    
+export {
+    addItem,
+    calculateTotal,
+    deleteItem
 }
 
