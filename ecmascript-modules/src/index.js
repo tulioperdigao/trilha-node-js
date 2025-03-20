441dc409b880e9fe2f83bded4ba82f1754f55de3
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-20 10:49:48 -0300

    Direct Export

--- ecmascript-modules/src/index.js
+++ ecmascript-modules/src/index.js
@@ -1,7 +1,10 @@
 import {disconnectDatabase, databaseType} from "./utils/database.js";
+import * as api from "./utils/api.js";
 
 
 console.log("Hello, Ecma!");
 
+api.getDataFromApi();
+console.log(api.key);
 disconnectDatabase();
 console.log(databaseType.userType);
---
+++ ecmascript-modules/src/utils/api.js
@@ -0,0 +1,8 @@
+export const key = {
+    value: "243984247239djfsfjk293901",
+    permission: "admin",
+};
+
+export const getDataFromApi = () => {
+    console.log("Dados da api sendo buscados...")
+}
