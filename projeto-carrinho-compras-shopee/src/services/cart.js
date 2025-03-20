0492358a13c322479525540cfef846871d67d0e5
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-20 15:31:52 -0300

    Assinaturas Métodos Cart

---
+++ projeto-carrinho-compras-shopee/src/services/cart.js
@@ -0,0 +1,23 @@
+// quais ações meu carrinho pode fazer
+
+// CASOS DE USO
+// -> adicionar Item no carrinho
+async function addItem(userCart) {
+    
+}
+
+// -> remover um Item - diminui um item
+async function removeItem(userCart, index) {
+    
+}
+
+// -> deletar Item do carrinho
+async function deleteItem(userCart, nameItem) {
+    
+}
+
+// -> calcular o total
+async function calculateTotal(userCart) {
+    
+}
+
