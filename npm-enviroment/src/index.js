2499229c196781771d36c0c5551afd3c61eac9d6
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-28 14:36:36 -0300

    multiple dotenv

--- npm-enviroment/src/index.js
+++ npm-enviroment/src/index.js
@@ -1,10 +1,12 @@
 import connectToDatabase from "./database/data.js";
 import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config({path:'.env.prod'});
+
+console.log(process.env.USERDATABASE);
 
 async function main() {
-    await connectToDatabase(process.env.USERDATABASE, process.env.PASSWORDDATABASE);
+await connectToDatabase("naruto", "uzumaki123");
 }
 
 main();
