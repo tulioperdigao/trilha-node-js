ec81d5607f6dae2e9e819abe5a3fccb22fc65e42
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-31 16:24:22 -0300

    qr-code generate

--- projeto-qrcode-generator/src/prompts/prompt-qrcode.js
+++ projeto-qrcode-generator/src/prompts/prompt-qrcode.js
@@ -1,5 +1,17 @@
 import chalk from "chalk";
 
-const promptQRCode = [];
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow("Digite o link para gerar o QR CODE: "),
+    },
+    {
+        name: "type",
+        description: chalk.yellow("Escolha o tipo (1 - IMAGEM) ou (2 - TERMINAL): "),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha apenas entre 1 e 2."),
+        required: true
+    }
+];
 
 export default promptQRCode;
