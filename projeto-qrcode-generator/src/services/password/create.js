a6d196ead6c63a5ea75c7de49c3a50e1456564bc
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-03-31 16:51:01 -0300

    Password generator

--- projeto-qrcode-generator/src/services/password/create.js
+++ projeto-qrcode-generator/src/services/password/create.js
@@ -1,7 +1,10 @@
 import chalk from "chalk";
+import handle from "./handle.js";
 
 async function createPassword() {
-    console.log(chalk.green("password"));
+    console.log(chalk.green("Senha gerada com sucesso:\n"));
+    const password = await handle()
+    console.log(password)
 }
 
 export default createPassword;
---
+++ projeto-qrcode-generator/src/services/password/handle.js
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function permittedCharacters() {
+    let permitted = [];
+
+    if(process.env.UPPERCASE_LETTERS === "true") {
+        permitted.push( ... "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+
+    if (process.env.LOWERCASE_LETTERS === "true") {
+        permitted.push( ... "abcdefghijklmnpqrstuvwxyz");
+    }
+
+    if (process.env.NUMBERS === "true") {
+        permitted.push( ... "0123456789");
+    }
+    if (process.env.SPECIAL_CHARACTERS === "true") {
+        permitted.push( ... "!@#$%¨&*()-_");
+    }
+
+    return permitted;
+}
+
+async function handle() {
+    let characters = [];
+    let password = "";
+
+    const passwordLength = process.env.PASSWORD_LENGTH
+    characters = await permittedCharacters();
+
+    for (let i = 0; i < passwordLength; i++) {
+        const index = Math.floor(Math.random() * characters.length);
+        password += characters[index]
+    }
+
+    return password;
+}
+
+export default handle;
