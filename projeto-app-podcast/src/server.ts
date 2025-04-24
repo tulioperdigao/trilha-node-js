e24218accc52e7cc38915b337059109f7a8582f5
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-04-24 14:16:25 -0300

    criando server http e camadas

--- projeto-app-podcast/src/server.ts
+++ projeto-app-podcast/src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+import * as http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+server.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}.`)
+})
