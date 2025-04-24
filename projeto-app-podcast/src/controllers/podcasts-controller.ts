8e578a081ec6a8f08035a2d5203c30ed9daaddc4
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-04-24 16:00:11 -0300

    chamada para API com simuladores de clients

--- projeto-app-podcast/src/controllers/podcasts-controller.ts
+++ projeto-app-podcast/src/controllers/podcasts-controller.ts
@@ -3,9 +3,20 @@ import {IncomingMessage, ServerResponse} from 'http';
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(200, {'content-type': 'application/json'});
-    res.end(
-        {
-            name: "Tugão"
-        }
-    )
+    res.end(JSON.stringify(
+        [
+            {
+                podcastName: "flow",
+                episode: "CBUM - Flow #319",
+                videoId: "pQSuQmUfS30",
+                categories: ["saúde", "bodybuilder", "esporte"]
+            },
+            {
+                podcastName: "flow",
+                episode: "SERGIO SACANI E BRENO MASI - Flow #434",
+                videoId: "9i3bTzbYHAg",
+                categories: ["ciência", "humor"]
+            }
+        ]
+    ))
 }
