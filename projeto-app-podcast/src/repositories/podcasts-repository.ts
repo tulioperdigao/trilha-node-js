f244db7b2776fe2a56f1ddaa6a5099b15463bcea
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-04-25 12:24:33 -0300

    modulo dinamico

---
+++ projeto-app-podcast/src/repositories/podcasts-repository.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import path from "path";
+import { Podcast } from "../models/podcast-model";
+
+
+
+const pathData = path.join(__dirname, "../repositories/podcasts.json");
+
+export const repositoryPodcast = async (): Promise<Podcast[]> => {
+    const rawData = fs.readFileSync(pathData, "utf-8")
+    const jsonFile = JSON.parse(rawData);
+
+    return jsonFile;
+}
--- projeto-app-podcast/src/services/list-episodes-service.ts
+++ projeto-app-podcast/src/services/list-episodes-service.ts
@@ -1,22 +1,9 @@
-
+import { repositoryPodcast } from "../repositories/podcasts-repository";
 
 
 
 export const serviceListEpisodes = async () => {
-    const data = [
-        {
-            podcastName: "flow",
-            episode: "CBUM - Flow #319",
-            videoId: "pQSuQmUfS30",
-            categories: ["saúde", "bodybuilder", "esporte"]
-        },
-        {
-            podcastName: "flow",
-            episode: "SERGIO SACANI E BRENO MASI - Flow #434",
-            videoId: "9i3bTzbYHAg",
-            categories: ["ciência", "humor"]
-        }
-    ]
+    const data = await repositoryPodcast();
 
     return data;
 }
