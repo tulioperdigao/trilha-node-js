ce2d470d6a8578b7ffc07f6b8d71a6393cf3ee41
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2025-05-01 18:51:37 -0300

    adicionando corredores e equipes

--- projeto-formula1/src/server.ts
+++ projeto-formula1/src/server.ts
@@ -2,14 +2,60 @@ import fastify from 'fastify';
 
 const server = fastify({ logger: true });
 
+const teams = [
+    {
+        id: 1,
+        name: 'Ferrari',
+        country: 'Itália'
+    },
+    {
+        id: 2,
+        name: 'McLaren',
+        country: 'Reino Unido'
+    },
+    {
+        id: 3,
+        name: 'Mercedes',
+        country: 'Alemanha'
+    },
+    {
+        id: 4,
+        name: 'Red Bull Racing',
+        country: 'Áustria'
+    }
+];
+
+const drivers = [
+    {
+        id: 1,
+        name: 'Max Verstappen',
+        team: 'Red Bull Racing'
+    },
+    {
+        id: 2,
+        name: 'Lewis Hamilton',
+        team: 'Mercedes'
+    },
+    {
+        id: 3,
+        name: 'Lando Norris',
+        team: 'McLaren'
+    },
+    {
+        id: 4,
+        name: 'Charles Leclerc',
+        team: 'Ferrari'
+    }
+]
+
 server.get('/teams', async(request, Response) => {
     Response.type("application/json").code(200);
-    return[
-        {
-            id: 1,
-            name: 'ferrari'
-        }
-    ]
+    return {teams};
+})
+
+server.get('/drivers', async (request, Response) => {
+    Response.type("application/json").code(200);
+    return {drivers}
 })
 
 server.listen({port: 3333}, () => {
